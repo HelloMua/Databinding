14bc841d320c84f8d428bb7481e167d6c5b359ea
HelloMua <jmua333@gmail.com>
2021-10-13 02:19:09 +0000

    Step4 : Create Two-Way Data Binding

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,8 @@
 sap.ui.require([
     "sap/m/Text",
-    "sap/ui/model/json/JSONModel"
-], function (Text, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView"
+], function (Text, JSONModel, XMLView) {
 	"use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
@@ -9,16 +10,19 @@ sap.ui.require([
 	sap.ui.getCore().attachInit(function () {
         // Create a JSON Model from an object literal
         var oModel = new JSONModel({
-            greetingText: "Hi, my name is Jeong Mua"
+            firstName: "Mua",
+            lastName: "Jeong",
+            enabled: true,
+            panelHeaderText: "Data Binding Basics"
         });
 
         // Assign the model object to the SAPUI5 core
         sap.ui.getCore().setModel(oModel);
         // this.getView().setModel(oModel);와 같은 의미
 
-		// Display a text element whose text is derived from the model object
-		new Text({
-            text: "{/greetingText}"
-        }).placeAt("content");      // index.html에 있는 body의 id와 연결
+        // Display the XML view called "App"
+        new XMLView({
+            viewName: "sap.ui.demo.db.view.App"
+        }).placeAt("content");
     });
 });
