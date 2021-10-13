7fd9600f01232bb4fa534b924e9dbd5798f6361e
HelloMua <jmua333@gmail.com>
2021-10-13 01:51:59 +0000

    Step2 : Creating a Model

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,21 @@
 sap.ui.require([
-	"sap/m/Text"
-], function (Text) {
+    "sap/m/Text",
+    "sap/ui/model/json/JSONModel"
+], function (Text, JSONModel) {
 	"use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
     // this.getView().getModel()
 	sap.ui.getCore().attachInit(function () {
+        // Create a JSON Model from an object literal
+        var oModel = new JSONModel({
+            greetingText: "Hi, my name is Jeong Mua"
+        });
+
+        // Assign the model object to the SAPUI5 core
+        sap.ui.getCore().setModel(oModel);
+        // this.getView().setModel(oModel);
+
 		// Create a text UI element that displays a hardcoded text string
 		new Text({
             text: "Hi, my name is Jeong Mua"
